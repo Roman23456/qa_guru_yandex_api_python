3b6543d79219fef3ed12005b1555b1a4f8c8e1fc
Roman Semenov <r_semenov@tochka.com>
2026-04-17 22:40:31 +0300

    Make config.py Jenkins-compatible with explicit API_KEY check

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # локально берёт из .env, в Jenkins из env variables
 
 base_url = "https://api.partner.market.yandex.ru/v2"
 api_key = os.getenv("API_KEY")
+
+if not api_key:
+    raise EnvironmentError("Переменная окружения API_KEY не задана")
 
